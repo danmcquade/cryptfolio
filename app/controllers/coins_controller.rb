505d6c3bfb06f56679b7a878548534730c8388d5
Dan McQuade <dtmcquade@gmail.com>
2018-01-09 14:57:58 -0500

    Add initial controller logic for coin data API

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,8 +1,26 @@
+require "net/http"
+
 class CoinsController < ApplicationController
 
   def index
-   @coins = Coin.all
-   render json: @coins
+    @coin_data = []
+    @coins = Coin.all
+    @coins.each do |coin|
+      url = "https://api.coinmarketcap.com/v1/ticker/#{coin.api_id}/"
+      resp = Net::HTTP.get_response(URI.parse(url))
+      data = JSON.parse(resp.body)
+      puts data
+      @coin_data.push(data)
+    end
+    render json: @coin_data
+  end
+
+  def show
+    @coin = Coin.find_by(api_id: params[:id])
+    url = "https://api.coinmarketcap.com/v1/ticker/#{params[:id]}/"
+    resp = Net::HTTP.get_response(URI.parse(url))
+    data = JSON.parse(resp.body)
+    render json: data
   end
 
 end
