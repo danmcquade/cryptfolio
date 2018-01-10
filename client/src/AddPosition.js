a0daad38651e791c833fc07503b397a0b13b2928
Dan McQuade <dtmcquade@gmail.com>
2018-01-10 17:46:16 -0500

    Update AddPosition component to allow for selecting from a list of supported currencies

--- client/src/AddPosition.js
+++ client/src/AddPosition.js
@@ -5,25 +5,25 @@ class AddPosition extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      position: {}
+      position: {},
+      currencies: []
     }
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-
   handleSubmit (e) {
-    let newPos = {}
+    let newPosition = {}
     e.preventDefault()
     let currency = e.target[0].value
     let shares = e.target[1].value
     let price = e.target[2].value
     let date = e.target[3].value
-    newPos = {currency: currency, shares: shares, purchase_price: price, purchase_date: date}
-    console.log(newPos)
+    newPosition = {currency: currency, shares: shares, purchase_price: price, purchase_date: date}
+    console.log(newPosition)
     const goBack = function () {
       this.props.history.push('/positions')
     }.bind(this)
-    this.setState({position: newPos}, () => {
+    this.setState({position: newPosition}, () => {
       let token = 'Bearer ' + localStorage.getItem('cryptfolio-jwt')
       $.ajax({
         url: 'http://localhost:3001/api/positions',
@@ -41,13 +41,34 @@ class AddPosition extends Component {
     })
   }
 
+  componentDidMount () {
+    $.ajax({
+      url: 'http://localhost:3001/api/coins/list',
+      type: 'GET',
+      context: this, // Allows us to use this.setState inside success
+      success: function (result) {
+        console.log(result)
+        this.setState({currencies: JSON.parse(JSON.stringify(result))})
+      },
+      error: function (xhr) {
+        console.log('Fetching API data failed')
+      }
+    })
+  }
+
   render () {
+    let currencies = this.state.currencies.map((c, index) => {
+      return (
+        <option key={index} value={c.symbol}>{c.name} ({c.symbol})</option>
+      )
+    })
+
     return (
       <div>
         <h1>New Position</h1>
         <button onClick={() => { this.click() }}>Click</button>
         <form onSubmit={this.handleSubmit}>
-          <p>Currency <input type='text' name='currency' /></p>
+          <p>Currency <select name='currency'>{currencies}</select></p>
           <p>Shares <input type='text' name='shares' /></p>
           <p>Purchase Price <input type='text' name='price' /></p>
           <p>Date <input type='text' name='date' /></p>
