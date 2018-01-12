1d22c20891606f411bfabfc4f7e3cb1244f82ea4
Dan McQuade <dtmcquade@gmail.com>
2018-01-12 10:06:16 -0500

    Remove dead code from EditModal component

--- client/src/EditModal.js
+++ client/src/EditModal.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 import $ from 'jquery'
-import Header from './Header'
 import './EditPosition.css'
 
 class EditModal extends Component {
