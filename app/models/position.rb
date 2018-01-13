d91de9a9e9ebe0d063593950f76e1c9c0eafdd4d
Dan McQuade <dtmcquade@gmail.com>
2018-01-13 14:41:46 -0500

    Update validations on positions model

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,6 @@
 class Position < ApplicationRecord
   belongs_to :user
+  validates :shares, :presence => true
+  validates :purchase_price, :presence => true
+  validates :purchase_date, :presence => true
 end
