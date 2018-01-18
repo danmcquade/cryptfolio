d01addf5deb9f62a76a23a832345decddda95eac
Dan McQuade <dtmcquade@gmail.com>
2018-01-18 13:37:31 -0500

    Update CORS access policy

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Cryptfolio
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'cryptfolio.danmcq.com'
         resource '*', :headers => :any, :methods => [:get, :post, :put, :options]
       end
     end
